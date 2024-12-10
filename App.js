b630b9d1c27eaa5f13f05e5c54778f3fbe9d7f85
IsaacMorocho <168015349+IsaacMorocho@users.noreply.github.com>
2024-12-10 10:01:52 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './si.png';
+import './App.css';
+import logo1 from './si.png'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo1" />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Joshua Morocho 
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
